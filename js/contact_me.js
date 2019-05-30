dc8d42e3b7db9cfe3c258072eac17dcc8a28bce1
sam smith <samsmith1@live.com>
2019-05-30 11:41:18 +1000

    corrected .alert-error

--- js/contact_me.js
+++ js/contact_me.js
@@ -41,10 +41,10 @@ $(function() {
                 error: function() {
                     // Fail message
                     $('#success').html("<div class='alert alert-success'>"); //danger script = <div class='alert alert-danger'>
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Thanks for contacting me " + firstName + ", an email has been sent. I will reply within 24 hours :) (if you need contact more urgently please call 0410419341)");
-                    $('#success > .alert-danger').append('</div>');
+                    $('#success > .alert-success').append("<strong>Thanks for contacting me " + firstName + ", an email has been sent. I will reply within 24 hours :) (if you need contact more urgently please call 0410419341)");
+                    $('#success > .alert-success').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
